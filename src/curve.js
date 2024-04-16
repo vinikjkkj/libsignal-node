22d751c48d8539bf644ee40805f4b67f5ce7291f
vinicius <viniwarrior4@gmail.com>
2024-04-15 22:04:50 -0300

    add validation in functions

--- src/curve.js
+++ src/curve.js
@@ -18,7 +18,7 @@ function validatePrivKey(privKey) {
         throw new Error("Undefined private key");
     }
     if (!(privKey instanceof Buffer)) {
-        throw new Error(`Invalid private key type: ${privKey.constructor.name}`);
+        throw new Error(`Invalid private key type: ${privKey?.constructor?.name}`);
     }
     if (privKey.byteLength != 32) {
         throw new Error(`Incorrect private key length: ${privKey.byteLength}`);
@@ -27,7 +27,7 @@ function validatePrivKey(privKey) {
 
 function scrubPubKeyFormat(pubKey) {
     if (!(pubKey instanceof Buffer)) {
-        throw new Error(`Invalid public key type: ${pubKey.constructor.name}`);
+        throw new Error(`Invalid public key type: ${pubKey?.constructor?.name}`);
     }
     if (pubKey === undefined || ((pubKey.byteLength != 33 || pubKey[0] != 5) && pubKey.byteLength != 32)) {
         throw new Error("Invalid public key");
