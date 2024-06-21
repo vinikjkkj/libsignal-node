142c5f84b5b6c9555d930558f092f7e82759a7de
vinicius <viniwarrior4@gmail.com>
2024-06-21 14:34:13 -0300

    inital commit (only tests)

--- src/crypto.js
+++ src/crypto.js
@@ -2,9 +2,8 @@
 
 'use strict';
 
-const nodeCrypto = require('crypto');
+const sodium = require('libsodium-wrappers')
 const assert = require('assert');
-
 
 function assertBuffer(value) {
     if (!(value instanceof Buffer)) {
@@ -13,69 +12,55 @@ function assertBuffer(value) {
     return value;
 }
 
-
 function encrypt(key, data, iv) {
-    assertBuffer(key);
-    assertBuffer(data);
-    assertBuffer(iv);
-    const cipher = nodeCrypto.createCipheriv('aes-256-cbc', key, iv);
-    return Buffer.concat([cipher.update(data), cipher.final()]);
+    const cipherText = sodium.crypto_secretbox_easy(data, iv, key);
+    return Buffer.from(cipherText);
 }
 
-
-function decrypt(key, data, iv) {
-    assertBuffer(key);
-    assertBuffer(data);
-    assertBuffer(iv);
-    const decipher = nodeCrypto.createDecipheriv('aes-256-cbc', key, iv);
-    return Buffer.concat([decipher.update(data), decipher.final()]);
+function decrypt(key, cipherText, iv) {
+    const decryptedData = sodium.crypto_secretbox_open_easy(cipherText, iv, key);
+    return Buffer.from(decryptedData);
 }
 
 
 function calculateMAC(key, data) {
-    assertBuffer(key);
-    assertBuffer(data);
-    const hmac = nodeCrypto.createHmac('sha256', key);
-    hmac.update(data);
-    return Buffer.from(hmac.digest());
+    if (key.length !== sodium.crypto_auth_KEYBYTES) {
+        throw new Error('Chave incorreta');
+    }
+
+    const mac = sodium.crypto_auth(data, key);
+    return Buffer.from(mac);
 }
 
 
 function hash(data) {
-    assertBuffer(data);
-    const sha512 = nodeCrypto.createHash('sha512');
-    sha512.update(data);
-    return sha512.digest();
+    const hash = sodium.crypto_generichash(data);
+    return Buffer.from(hash);
 }
 
 
 // Salts always end up being 32 bytes
 function deriveSecrets(input, salt, info, chunks) {
-    // Specific implementation of RFC 5869 that only returns the first 3 32-byte chunks
-    assertBuffer(input);
-    assertBuffer(salt);
-    assertBuffer(info);
-    if (salt.byteLength != 32) {
-        throw new Error("Got salt of incorrect length");
+    if (salt.length !== sodium.crypto_generichash_BYTES) {
+        throw new Error('Salt incorreto');
     }
+
     chunks = chunks || 3;
-    assert(chunks >= 1 && chunks <= 3);
-    const PRK = calculateMAC(salt, input);
-    const infoArray = new Uint8Array(info.byteLength + 1 + 32);
-    infoArray.set(info, 32);
-    infoArray[infoArray.length - 1] = 1;
-    const signed = [calculateMAC(PRK, Buffer.from(infoArray.slice(32)))];
-    if (chunks > 1) {
-        infoArray.set(signed[signed.length - 1]);
-        infoArray[infoArray.length - 1] = 2;
-        signed.push(calculateMAC(PRK, Buffer.from(infoArray)));
+    if (chunks < 1 || chunks > 3) {
+        throw new Error('Chunks deve ser um número entre 1 e 3');
     }
-    if (chunks > 2) {
-        infoArray.set(signed[signed.length - 1]);
-        infoArray[infoArray.length - 1] = 3;
-        signed.push(calculateMAC(PRK, Buffer.from(infoArray)));
+
+    const PRK = sodium.crypto_generichash(sodium.crypto_generichash_BYTES, input, salt);
+    const derivedSecrets = [];
+
+    for (let i = 1; i <= chunks; i++) {
+        const infoArray = Buffer.concat([info, Buffer.from([i])]);
+        const derivedSecret = sodium.crypto_generichash(sodium.crypto_generichash_BYTES, infoArray, PRK);
+
+        derivedSecrets.push(Buffer.from(derivedSecret));
     }
-    return signed;
+
+    return derivedSecrets;
 }
 
 function verifyMAC(data, key, mac, length) {
